fd4fce81adccab260f6f68dceb740cfef981d1c0
sachioross <sachioross@gmail.com>
2024-11-11 13:57:54 -0500

    Supporting new 'aem.live' pattern as well as previous 'hlx' pattern

--- scripts/utils.js
+++ scripts/utils.js
@@ -19,7 +19,7 @@ export const [setLibs, getLibs] = (() => {
     (prodLibs, location) => {
       libs = (() => {
         const { hostname, search } = location || window.location;
-        if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
+        if (!(hostname.includes('.hlx.') || hostname.includes('local') || hostname.includes('.aem.'))) return prodLibs;
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
         if (branch === 'local') return 'http://localhost:6456/libs';
         return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
